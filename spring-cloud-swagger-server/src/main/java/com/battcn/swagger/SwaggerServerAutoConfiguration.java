8d5dfc9333fb2099cb5b0790c11bfff155e5e318
Levin <1837307557@qq.com>
2017-12-31 10:28:21 +0800

    优化配置细节

--- spring-cloud-swagger-server/src/main/java/com/battcn/swagger/SwaggerServerAutoConfiguration.java
+++ spring-cloud-swagger-server/src/main/java/com/battcn/swagger/SwaggerServerAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.battcn.swagger;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * @author Levin
  */
 @Configuration
+@ComponentScan("com.battcn.swagger.controller")
 public class SwaggerServerAutoConfiguration extends WebMvcConfigurerAdapter {
 
 
