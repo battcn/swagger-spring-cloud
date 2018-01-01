1612a803a91141bb2fbf07c3d85d790a70eab0bc
Levin <1837307557@qq.com>
2018-01-01 13:07:46 +0800

    优化配置细节

--- spring-cloud-swagger-server/src/main/java/com/battcn/swagger/config/ServletContextConfig.java
+++ spring-cloud-swagger-server/src/main/java/com/battcn/swagger/config/ServletContextConfig.java
@@ -14,7 +14,7 @@ public class ServletContextConfig extends WebMvcConfigurationSupport {
      * 发现如果继承了WebMvcConfigurationSupport，则在yml中配置的相关内容会失效。
      * 需要重新指定静态资源
      *
-     * @param registry
+     * @param registry 注册
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
