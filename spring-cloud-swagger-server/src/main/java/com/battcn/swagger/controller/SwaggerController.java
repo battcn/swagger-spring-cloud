460ad306aeb4cb57cd4a1fcd3d3eb8fe03cc783f
Levin <1837307557@qq.com>
2018-01-04 16:22:49 +0800

    修正服务down机时请求失败

--- spring-cloud-swagger-server/src/main/java/com/battcn/swagger/controller/SwaggerController.java
+++ spring-cloud-swagger-server/src/main/java/com/battcn/swagger/controller/SwaggerController.java
@@ -1,9 +1,11 @@
 package com.battcn.swagger.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.battcn.swagger.model.CloudSwaggerResource;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -15,6 +17,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger.web.SwaggerResource;
 
@@ -51,6 +54,13 @@ public class SwaggerController {
                 continue;
             }
             for (ServiceInstance service : instances) {
+                log.info(JSON.toJSONString(service));
+                JSONObject object = JSON.parseObject(JSON.toJSONString(service));
+                JSONObject instanceInfo = object.getJSONObject("instanceInfo");
+                String status = instanceInfo.getString("status");
+                if (!StringUtils.equalsIgnoreCase(status, "UP")) {
+                    continue;
+                }
                 List<SwaggerResource> swaggerResourceArrayList = Lists.newArrayList();
                 try {
                     ResponseEntity<String> responseEntity = this.restTemplate.exchange("http://" + serviceId + "/swagger-resources", HttpMethod.GET, null, String.class);
@@ -68,8 +78,14 @@ public class SwaggerController {
                     cloudSwaggerResource.setServiceInstances(instances);
                     cloudSwaggerResource.setSwaggerResources(swaggerResourceArrayList);
                     list.add(cloudSwaggerResource);
-                } catch (HttpClientErrorException e) {
+                } catch (HttpClientErrorException e1) {
                     log.error("[{}] - [404] - [已跳过]", serviceId);
+                } catch (Exception ex) {
+                    if (ex instanceof java.net.ConnectException || ex instanceof org.springframework.web.client.ResourceAccessException) {
+                        log.error("[{}] - [服务DOWN] - [已跳过]", serviceId);
+                    } else {
+                        log.error("[{}] - [{}]", ex);
+                    }
                 }
             }
         }
